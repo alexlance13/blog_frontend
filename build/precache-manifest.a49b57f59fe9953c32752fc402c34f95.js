53bd5f48fa0d382ba5eb53549eadcac6fe8bcc50
Alex Lance <alexlance@Alexs-MacBook-Air.local>
2019-12-26 19:18:08 +0200

    Up to heroku

---
+++ build/precache-manifest.a49b57f59fe9953c32752fc402c34f95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb0b5323a83f108aac8d42f008dd13c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8bb8728c4875e00b0b",
+    "url": "/static/css/main.2a397846.chunk.css"
+  },
+  {
+    "revision": "a26ee6e94797996f4d53",
+    "url": "/static/js/2.562c6c2f.chunk.js"
+  },
+  {
+    "revision": "31fb39f0bc749d2806122d0d412587e5",
+    "url": "/static/js/2.562c6c2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a8bb8728c4875e00b0b",
+    "url": "/static/js/main.2715f495.chunk.js"
+  },
+  {
+    "revision": "a348205103e6096a3248",
+    "url": "/static/js/runtime-main.df0c6884.js"
+  },
+  {
+    "revision": "afbd595ed462e39a93a3b676aa2ea7a4",
+    "url": "/static/media/home-bg.afbd595e.jpg"
+  }
+]);
